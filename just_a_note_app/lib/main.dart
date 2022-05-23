03ecb766b3b382fe2c7ec2a5f2546f5ed030fe26
mervekokmen <mervekokmen222@gmail.com>
2022-05-23 23:06:31 +0300

    Buton, Tema, Icon tasarım dosyaları oluşturuldu. Main içinde basit ana ekran oluşturuldu.
    Buton, Tema, Icon tasarım dosyaları oluşturuldu. Main içinde basit ana ekran oluşturuldu. Pubspec.yaml dosyasına pluginler eklendi.

--- just_a_note_app/lib/main.dart
+++ just_a_note_app/lib/main.dart
@@ -1,66 +1,124 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'flutter_flow/flutter_flow_theme.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>();
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale _locale;
+  ThemeMode _themeMode = ThemeMode.system;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void setLocale(Locale value) => setState(() => _locale = value);
+  void setThemeMode(ThemeMode mode) => setState(() {
+        _themeMode = mode;
+      });
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Just A Note',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: _locale,
+      supportedLocales: const [Locale('en', '')],
+      theme: ThemeData(brightness: Brightness.light),
+      themeMode: _themeMode,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class NavBarPage extends StatefulWidget {
+  NavBarPage({Key key, this.initialPage}) : super(key: key);
 
-  final String title;
+  final String initialPage;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _NavBarPageState createState() => _NavBarPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+/// This is the private State class that goes with NavBarPage.
+class _NavBarPageState extends State<NavBarPage> {
+  String _currentPage = 'myTasks';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _currentPage = widget.initialPage ?? _currentPage;
   }
 
   @override
   Widget build(BuildContext context) {
+    final tabs = {
+      /*
+      'CompletedTasks': CompletedTasksWidget(),
+      'myTasks': MyTasksWidget(),
+      'MyProfile': MyProfileWidget(),
+      */
+    };
+    final currentIndex = tabs.keys.toList().indexOf(_currentPage);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: tabs[_currentPage],
+      bottomNavigationBar: GNav(
+        selectedIndex: currentIndex,
+        onTabChange: (i) =>
+            setState(() => _currentPage = tabs.keys.toList()[i]),
+        backgroundColor: FlutterFlowTheme.of(context).primaryBlack,
+        color: FlutterFlowTheme.of(context).tertiaryColor,
+        activeColor: FlutterFlowTheme.of(context).primaryColor,
+        tabBackgroundColor: Color(0x00000000),
+        tabBorderRadius: 100,
+        tabMargin: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+        gap: 0,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        duration: Duration(milliseconds: 500),
+        haptic: false,
+        tabs: [
+          GButton(
+            icon: currentIndex == 0 ? Icons.alarm_on : Icons.alarm_on,
+            text: 'Completed',
+            iconSize: 32,
+          ),
+          GButton(
+            icon: currentIndex == 1 ? Icons.playlist_add : Icons.playlist_add,
+            text: 'My Tasks',
+            iconSize: 32,
+          ),
+          GButton(
+            icon: currentIndex == 2 ? Icons.person_sharp : Icons.person_outline,
+            text: 'Home',
+            iconSize: 32,
+          )
+        ],
       ),
     );
   }
